eba4292b1137ac33e340e60ae639678aa202fbf3
David Voong <voong.david@gmail.com>
2016-01-16 11:55:10 +0000

    Fix routing: Fix middleware ordering

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express();
 var routes = require('./routes');
 
 var errorHandlers = require('./middleware/errorhandlers');
-app.use(errorHandlers.notFound);
 
 app.get('/', routes.index);
 app.get('/login', routes.login);
 app.post('/login', routes.loginProcess);
 app.get('/chat', routes.chat);
+app.use(errorHandlers.notFound);
 
 app.listen(3000);
 console.log("App server running on port 3000");
