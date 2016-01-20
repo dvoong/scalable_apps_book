5c28ab591fef54338312b5a9c43a5e38d882b3b8
David Voong <voong.david@gmail.com>
2016-01-20 07:51:24 +0000

    Add basic frontend interactivity

--- socket_app/app.js
+++ socket_app/app.js
@@ -1,10 +1,16 @@
 var io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket){
-    socket.emit('ping');
+    
+    socket.on('join', function(data){
+	io.sockets.emit('userJoined', data);
+	socket.username = data.username;
+    });
 
-    socket.on('pong', function(data){
-	console.log('pong');
+    socket.on('ping', function(data, done){
+	io.sockets.emit('ping', {username: socket.username});
+	console.log('received ping, data: ' + data);
+	done('ack');
     });
 });
 
