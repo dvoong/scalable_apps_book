e51a150ca8e3762840f41e269e1a018fb4317eaa
David Voong <voong.david@gmail.com>
2016-01-23 19:08:25 +0000

    Add broadcasting to socket events

--- socket_app/app.js
+++ socket_app/app.js
@@ -3,14 +3,14 @@ var io = require('socket.io').listen(4000);
 io.sockets.on('connection', function(socket){
     
     socket.on('join', function(data){
-	io.sockets.emit('userJoined', data);
+	socket.broadcast.emit('userJoined', data);
 	socket.username = data.username;
     });
 
     socket.on('ping', function(data, done){
-	io.sockets.emit('ping', {username: socket.username});
-	console.log('received ping, data: ' + data);
+	socket.broadcast.emit('ping', {username: socket.username});
 	done('ack');
     });
+
 });
 
